1a30faefaf3931b9636af4f59e0a1bb3e61af855
Guillermo Campelo <guicamest@gmail.com>
2012-05-24 00:58:44 -0300

    Added swt 3.7.2 files

--- FileDuplicateFinderUI/config.groovy
+++ FileDuplicateFinderUI/config.groovy
@@ -1,35 +1,33 @@
 // File: config.groovy
 environments  {
 	
+	allLibs = 'libs'
+	eclipseVersion = '3.7.2'
+	swtLibs = "${allLibs}/${eclipseVersion}"
+
 	windows32 {
-		swtLibs = 'libs'
 		swtOsJars = "${swtLibs}/win_x86"
 	}
 	
 	windows64 {
-		swtLibs = 'libs'
 		swtOsJars = "${swtLibs}/win_x86_64"
 	}
 	
 	linux32 {			
-		swtLibs = 'libs'
 		swtOsJars = "${swtLibs}/linux_x86"
 	}
 	
 	linux64 {
-		swtLibs = 'libs'
 		swtOsJars = "${swtLibs}/linux_x86_64"
 	}
 	
 	// Oldest one
 	mac-carbon {
-		swtLibs = 'libs'
 		swtOsJars = "${swtLibs}/mac_carbon"
 	}
 	
 	// Newest one
 	mac-cocoa {
-		swtLibs = 'libs'
 		swtOsJars = "${swtLibs}/mac_cocoa"	
 	}
 }
