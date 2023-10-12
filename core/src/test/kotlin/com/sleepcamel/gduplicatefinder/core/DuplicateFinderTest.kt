f07b72784ac6a1bc82af5aa975bac3cf44d42c52
guicamest <guicamest@gmail.com>
2023-10-12 20:18:19 +0200

    [skip ci]: Add `directory does not exist` test

--- core/src/test/kotlin/com/sleepcamel/gduplicatefinder/core/DuplicateFinderTest.kt
+++ core/src/test/kotlin/com/sleepcamel/gduplicatefinder/core/DuplicateFinderTest.kt
@@ -1,14 +1,30 @@
 package com.sleepcamel.gduplicatefinder.core
 
-import org.junit.jupiter.api.Assertions.assertTrue
+import kotlinx.coroutines.test.runTest
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Nested
+import java.nio.file.Paths
+import kotlin.test.Test
 
 @DisplayName("DuplicateFinder should")
 class DuplicateFinderTest {
 
-    @Test
-    fun simple(){
-        assertTrue(true)
+    val duplicateFinder = SequentialDuplicateFinder()
+
+    @DisplayName("not detect duplicates when")
+    @Nested
+    inner class NoDuplicates {
+
+        @Test
+        fun `directory does not exist`(){
+            val nonExistentDirectory = Paths.get("nonExistentDirectory")
+            val execution = duplicateFinder.find(listOf(nonExistentDirectory))
+
+            runTest {
+                assertThat(execution.duplicateEntries()).isEmpty()
+            }
+        }
     }
+
 }
