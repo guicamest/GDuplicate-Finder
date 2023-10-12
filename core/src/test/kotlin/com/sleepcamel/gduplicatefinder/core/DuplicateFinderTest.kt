addca492c50f4cd6947211f8a3466d13d896b169
guicamest <guicamest@gmail.com>
2023-10-12 20:48:48 +0200

    chore: test `directory exists but has two different files`

--- core/src/test/kotlin/com/sleepcamel/gduplicatefinder/core/DuplicateFinderTest.kt
+++ core/src/test/kotlin/com/sleepcamel/gduplicatefinder/core/DuplicateFinderTest.kt
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import java.nio.file.Paths
 import kotlin.io.path.createTempDirectory
+import kotlin.io.path.createTempFile
+import kotlin.io.path.writeText
 import kotlin.test.Test
 
 @DisplayName("DuplicateFinder should")
@@ -38,6 +40,30 @@ class DuplicateFinderTest {
                 assertThat(execution.duplicateEntries()).isEmpty()
             }
         }
+
+        @Test
+        fun `directory exists but has only one file`(){
+            val directoryWithOneFile = createTempDirectory("directoryWithOneFile")
+            assertThat(createTempFile(directory = directoryWithOneFile)).exists().isRegularFile()
+
+            val execution = duplicateFinder.find(listOf(directoryWithOneFile))
+            runTest {
+                assertThat(execution.duplicateEntries()).isEmpty()
+            }
+        }
+
+        @Test
+        fun `directory exists but has two different files`(){
+            val directoryWith2DifferentFiles = createTempDirectory("directoryWith2DifferentFiles")
+            createTempFile(directory = directoryWith2DifferentFiles).writeText("hi")
+            createTempFile(directory = directoryWith2DifferentFiles).writeText("bye")
+
+            val execution = duplicateFinder.find(listOf(directoryWith2DifferentFiles))
+            runTest {
+                assertThat(execution.duplicateEntries()).isEmpty()
+            }
+        }
+
     }
 
 }
