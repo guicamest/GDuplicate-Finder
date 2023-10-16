6ffc087ac5bab796c7416bcc97ffc546e573c411
guicamest <guicamest@gmail.com>
2023-10-17 00:32:23 +0200

    style: Add .editorconfig and apply new format

--- core/src/main/kotlin/com/sleepcamel/gduplicatefinder/core/SequentialDuplicateFinder.kt
+++ core/src/main/kotlin/com/sleepcamel/gduplicatefinder/core/SequentialDuplicateFinder.kt
@@ -4,5 +4,6 @@ import kotlinx.coroutines.CoroutineScope
 import java.nio.file.Path
 
 class SequentialDuplicateFinder(private val parentScope: CoroutineScope) : DuplicateFinder {
-    override fun find(directories: Collection<Path>): FindDuplicatesExecution = CoroutinesFindDuplicatesExecution(directories, parentScope)
+    override fun find(directories: Collection<Path>): FindDuplicatesExecution =
+        CoroutinesFindDuplicatesExecution(directories, parentScope)
 }
