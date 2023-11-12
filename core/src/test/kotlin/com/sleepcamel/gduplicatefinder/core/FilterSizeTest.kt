048d8586350fafcc9d5a6d6e80856727a3922913
guicamest <guicamest@gmail.com>
2023-11-12 15:07:38 +0100

    test: `DuplicateFinder should detect duplicates when duplicate files size is more than minSize`

--- core/src/test/kotlin/com/sleepcamel/gduplicatefinder/core/FilterSizeTest.kt
+++ core/src/test/kotlin/com/sleepcamel/gduplicatefinder/core/FilterSizeTest.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.test.runTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
+import kotlin.io.path.absolute
 import kotlin.io.path.createTempDirectory
 import kotlin.io.path.createTempFile
 import kotlin.io.path.writeText
@@ -34,6 +35,42 @@ class FilterSizeTest {
 
                 val duplicateEntries = execution.duplicateEntries()
                 assertThat(duplicateEntries).isEmpty()
+            }
+        }
+    }
+
+    @DisplayName("detect duplicates when")
+    @Nested
+    inner class Duplicates {
+        @Test
+        fun `duplicate files size is more than minSize`() {
+            val directoryWith2FilesSameHash = createTempDirectory("directoryWith2FilesSameHash")
+            val fileNames =
+                (0..1).map {
+                    createTempFile(directory = directoryWith2FilesSameHash).apply {
+                        writeText("hi")
+                    }.absolute()
+                }
+            val expectedDuplicateGroup =
+                DuplicateGroup(
+                    hash = "hi".contentHash(),
+                    paths = fileNames,
+                )
+
+            runTest {
+                val execution =
+                    findDuplicates(
+                        parentScope = this,
+                        directories = listOf(directoryWith2FilesSameHash),
+                        filter = MinSizeFilter(1L),
+                    )
+
+                val duplicateEntries = execution.duplicateEntries()
+                assertThat(duplicateEntries).hasSize(1)
+                duplicateEntries.first().also { group ->
+                    assertThat(group.hash).isEqualTo(expectedDuplicateGroup.hash)
+                    assertThat(group.paths).containsExactlyInAnyOrderElementsOf(expectedDuplicateGroup.paths)
+                }
             }
         }
     }
