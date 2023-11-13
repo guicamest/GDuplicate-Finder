6cda462c361a02e8ab9a2757d7042aa507dd17a8
guicamest <guicamest@gmail.com>
2023-11-13 14:05:30 +0100

    refactor(FilterSizeTest): Rewrite tests with Parametrized

--- core/src/test/kotlin/com/sleepcamel/gduplicatefinder/core/FilterSizeTest.kt
+++ core/src/test/kotlin/com/sleepcamel/gduplicatefinder/core/FilterSizeTest.kt
@@ -3,84 +3,42 @@ package com.sleepcamel.gduplicatefinder.core
 import kotlinx.coroutines.test.runTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Nested
-import kotlin.test.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 @DisplayName("DuplicateFinder should")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class FilterSizeTest {
-    @DisplayName("not detect duplicates when")
-    @Nested
-    inner class NoDuplicates {
-        @Test
-        fun `duplicate files size is less than minSize`() {
-            val (directory, _) = createDuplicates(content = "some content")
+    @DisplayName("detect when content size is 12")
+    @ParameterizedTest(name = "{1} duplicate groups with filter {0}")
+    @MethodSource("sizeFilters")
+    fun `parametrized filter test`(
+        filter: PathFilter,
+        expectedDuplicateGroups: Int,
+    ) {
+        val (directory, _) = createDuplicates(content = "some content")
 
-            runTest {
-                val execution =
-                    findDuplicates(
-                        parentScope = this,
-                        directory = directory,
-                        filter = MinSizeFilter(100L),
-                    )
+        runTest {
+            val execution =
+                findDuplicates(
+                    parentScope = this,
+                    directory = directory,
+                    filter = filter,
+                )
 
-                val duplicateEntries = execution.duplicateEntries()
-                assertThat(duplicateEntries).isEmpty()
-            }
-        }
-
-        @Test
-        fun `duplicate files size is more than maxSize`() {
-            val (directory, _) = createDuplicates(content = "some content")
-
-            runTest {
-                val execution =
-                    findDuplicates(
-                        parentScope = this,
-                        directory = directory,
-                        filter = MaxSizeFilter(5L),
-                    )
-
-                val duplicateEntries = execution.duplicateEntries()
-                assertThat(duplicateEntries).isEmpty()
-            }
+            val duplicateEntries = execution.duplicateEntries()
+            assertThat(duplicateEntries).hasSize(expectedDuplicateGroups)
         }
     }
 
-    @DisplayName("detect duplicates when")
-    @Nested
-    inner class Duplicates {
-        @Test
-        fun `duplicate files size is more than minSize`() {
-            val (directory, _) = createDuplicates(content = "some content")
-
-            runTest {
-                val execution =
-                    findDuplicates(
-                        parentScope = this,
-                        directory = directory,
-                        filter = MinSizeFilter(1L),
-                    )
-
-                val duplicateEntries = execution.duplicateEntries()
-                assertThat(duplicateEntries).hasSize(1)
-            }
-        }
-
-        @Test
-        fun `duplicate files size is less than maxSize`() {
-            val (directory, _) = createDuplicates(content = "some content")
-
-            runTest {
-                val execution =
-                    findDuplicates(
-                        parentScope = this,
-                        directory = directory,
-                        filter = MaxSizeFilter(100L),
-                    )
-
-                val duplicateEntries = execution.duplicateEntries()
-                assertThat(duplicateEntries).hasSize(1)
-            }
-        }
-    }
+    private fun sizeFilters() =
+        Stream.of(
+            arguments(MinSizeFilter(100), 0),
+            arguments(MinSizeFilter(8), 1),
+            arguments(MaxSizeFilter(100), 1),
+            arguments(MaxSizeFilter(8), 0),
+        )
 }
