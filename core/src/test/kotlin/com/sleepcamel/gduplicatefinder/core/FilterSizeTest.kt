ec3f3f17ec7eac89dff95f7d8159739bc81451f4
guicamest <guicamest@gmail.com>
2023-11-13 12:06:55 +0100

    refactor(FilterSizeTest): Use `createDuplicates` function in tests

--- core/src/test/kotlin/com/sleepcamel/gduplicatefinder/core/FilterSizeTest.kt
+++ core/src/test/kotlin/com/sleepcamel/gduplicatefinder/core/FilterSizeTest.kt
@@ -5,10 +5,6 @@ import kotlinx.coroutines.test.runTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
-import kotlin.io.path.absolute
-import kotlin.io.path.createTempDirectory
-import kotlin.io.path.createTempFile
-import kotlin.io.path.writeText
 import kotlin.test.Test
 
 @DisplayName("DuplicateFinder should")
@@ -18,19 +14,14 @@ class FilterSizeTest {
     inner class NoDuplicates {
         @Test
         fun `duplicate files size is less than minSize`() {
-            val directoryWith2FilesSameHash = createTempDirectory("directoryWith2FilesSameHash")
-            repeat(2) {
-                createTempFile(directory = directoryWith2FilesSameHash).apply {
-                    writeText("hi")
-                }
-            }
+            val (directory, _) = createDuplicates(content = "some content")
 
             runTest {
                 val execution =
                     findDuplicates(
                         parentScope = this,
-                        directories = listOf(directoryWith2FilesSameHash),
-                        filter = MinSizeFilter(3L),
+                        directory = directory,
+                        filter = MinSizeFilter(100L),
                     )
 
                 val duplicateEntries = execution.duplicateEntries()
@@ -44,33 +35,18 @@ class FilterSizeTest {
     inner class Duplicates {
         @Test
         fun `duplicate files size is more than minSize`() {
-            val directoryWith2FilesSameHash = createTempDirectory("directoryWith2FilesSameHash")
-            val fileNames =
-                (0..1).map {
-                    createTempFile(directory = directoryWith2FilesSameHash).apply {
-                        writeText("hi")
-                    }.absolute()
-                }
-            val expectedDuplicateGroup =
-                DuplicateGroup(
-                    hash = "hi".contentHash(),
-                    paths = fileNames,
-                )
+            val (directory, _) = createDuplicates(content = "some content")
 
             runTest {
                 val execution =
                     findDuplicates(
                         parentScope = this,
-                        directories = listOf(directoryWith2FilesSameHash),
+                        directory = directory,
                         filter = MinSizeFilter(1L),
                     )
 
                 val duplicateEntries = execution.duplicateEntries()
                 assertThat(duplicateEntries).hasSize(1)
-                duplicateEntries.first().also { group ->
-                    assertThat(group.hash).isEqualTo(expectedDuplicateGroup.hash)
-                    assertThat(group.paths).containsExactlyInAnyOrderElementsOf(expectedDuplicateGroup.paths)
-                }
             }
         }
     }
