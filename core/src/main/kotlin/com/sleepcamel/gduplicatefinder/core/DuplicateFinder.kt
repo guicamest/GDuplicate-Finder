67d75e8159b6b9ac33c676c9d60fe7b4873a8b47
guicamest <guicamest@gmail.com>
2023-11-13 12:26:08 +0100

    test: `DuplicateFinder should not detect duplicates when duplicate files size is more than maxSize`

--- core/src/main/kotlin/com/sleepcamel/gduplicatefinder/core/DuplicateFinder.kt
+++ core/src/main/kotlin/com/sleepcamel/gduplicatefinder/core/DuplicateFinder.kt
@@ -1,11 +1,10 @@
 package com.sleepcamel.gduplicatefinder.core
 
-import com.sleepcamel.gduplicatefinder.core.pathfilter.MinSizeFilter
 import java.nio.file.Path
 
 interface DuplicateFinder {
     fun find(
         directories: Collection<Path>,
-        filter: MinSizeFilter,
+        filter: PathFilter,
     ): FindDuplicatesExecution
 }
---
+++ core/src/main/kotlin/com/sleepcamel/gduplicatefinder/core/PathFilters.kt
@@ -0,0 +1,29 @@
+package com.sleepcamel.gduplicatefinder.core
+
+import java.io.IOException
+import java.nio.file.Path
+import java.nio.file.attribute.BasicFileAttributes
+
+interface PathFilter {
+    @Throws(IOException::class)
+    fun accept(
+        entry: Path,
+        attributes: BasicFileAttributes,
+    ): Boolean
+}
+
+class MinSizeFilter(private val size: Long) : PathFilter {
+    @Throws(IOException::class)
+    override fun accept(
+        entry: Path,
+        attributes: BasicFileAttributes,
+    ) = attributes.size() > size
+}
+
+class MaxSizeFilter(private val size: Long) : PathFilter {
+    @Throws(IOException::class)
+    override fun accept(
+        entry: Path,
+        attributes: BasicFileAttributes,
+    ) = attributes.size() < size
+}
