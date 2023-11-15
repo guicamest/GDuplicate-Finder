3346417c50f5d0e9ca6f04ef15792c2a236e2413
guicamest <guicamest@gmail.com>
2023-11-15 10:40:52 +0100

    test(FiltersTest): filename without extension filter

--- core/src/test/kotlin/com/sleepcamel/gduplicatefinder/core/FiltersTest.kt
+++ core/src/test/kotlin/com/sleepcamel/gduplicatefinder/core/FiltersTest.kt
@@ -11,11 +11,11 @@ import java.util.stream.Stream
 
 @DisplayName("DuplicateFinder should")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class FilterSizeTest {
+class FiltersTest {
     @DisplayName("detect when content size is 12")
     @ParameterizedTest(name = "{1} duplicate groups with filter {0}")
     @MethodSource("sizeFilters")
-    fun `parametrized filter test`(
+    fun `parametrized size filter test`(
         filter: PathFilter,
         expectedDuplicateGroups: Int,
     ) {
@@ -40,5 +40,34 @@ class FilterSizeTest {
             arguments(MinSizeFilter(8), 1),
             arguments(MaxSizeFilter(100), 1),
             arguments(MaxSizeFilter(8), 0),
+        )
+
+    @DisplayName("detect when filename is somefile*.txt")
+    @ParameterizedTest(name = "{1} duplicate groups with filter {0}")
+    @MethodSource("nameFilters")
+    fun `parametrized file name with extension filter test`(
+        filter: PathFilter,
+        expectedDuplicateGroups: Int,
+    ) {
+        val (directory, _) = createDuplicates(filenamePrefixAndSuffix = "somefile" to ".txt")
+
+        runTest {
+            val execution =
+                findDuplicates(
+                    parentScope = this,
+                    directory = directory,
+                    filter = filter,
+                )
+
+            val duplicateEntries = execution.duplicateEntries()
+            assertThat(duplicateEntries).hasSize(expectedDuplicateGroups)
+        }
+    }
+
+    private fun nameFilters() =
+        Stream.of(
+            arguments(FilenameFilter("somefile"), 1),
+            arguments(FilenameFilter("donotfind"), 0),
+            arguments(FilenameFilter("somefile.txt"), 0),
         )
 }
