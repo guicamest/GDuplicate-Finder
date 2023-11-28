a367f3009fe14982ffefc52110907dbad7fcf647
guicamest <guicamest@gmail.com>
2023-11-28 15:16:43 +0100

    test(FiltersTest): directory name filter [skip ci]

--- core/src/test/kotlin/com/sleepcamel/gduplicatefinder/core/FiltersTest.kt
+++ core/src/test/kotlin/com/sleepcamel/gduplicatefinder/core/FiltersTest.kt
@@ -8,6 +8,8 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments.arguments
 import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
+import kotlin.io.path.createDirectory
+import kotlin.io.path.createTempDirectory
 import kotlin.io.path.listDirectoryEntries
 
 @DisplayName("DuplicateFinder should")
@@ -93,5 +95,37 @@ class FiltersTest {
             arguments(FullFilenameFilter(name = "somefile.*\\.txt", exact = true), 1),
             arguments(FullFilenameFilter(name = "e.*\\.t", exact = false), 1),
             arguments(FullFilenameFilter(name = "e.*\\.t", exact = true), 0),
+        )
+
+    @DisplayName("detect when file is inside directory named paramDir")
+    @ParameterizedTest(name = "{1} duplicate groups with filter {0}")
+    @MethodSource("directoryFilters")
+    fun `parametrized directory name with extension filter test`(
+        filter: PathFilter,
+        expectedDuplicateGroups: Int,
+    ) {
+        val temp = createTempDirectory()
+        val (directory, _) = createDuplicates(directory = temp.resolve("paramDir").createDirectory())
+
+        runTest {
+            val execution =
+                findDuplicates(
+                    parentScope = this,
+                    directory = directory,
+                    filter = filter,
+                )
+
+            val duplicateEntries = execution.duplicateEntries()
+            assertThat(duplicateEntries)
+                .withRepresentation {
+                    "$it\nFiles in directory: ${directory.listDirectoryEntries().joinToString()}"
+                }
+                .hasSize(expectedDuplicateGroups)
+        }
+    }
+
+    private fun directoryFilters() =
+        Stream.of(
+            arguments(DirectoryFilter(name = "somefile", exact = false), 0),
         )
 }
