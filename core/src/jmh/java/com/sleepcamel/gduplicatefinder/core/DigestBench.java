9afd3ffcd11380fe5d647f329ba3d168e320507f
guicamest <guicamest@gmail.com>
2023-11-28 21:58:06 +0100

    chore(jmh): Rename variables, change state scope to Benchmark

--- core/src/jmh/java/com/sleepcamel/gduplicatefinder/core/DigestBench.java
+++ core/src/jmh/java/com/sleepcamel/gduplicatefinder/core/DigestBench.java
@@ -15,7 +15,7 @@ import java.util.function.BiConsumer;
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @Fork(value = 1)
-@State(Scope.Thread)
+@State(Scope.Benchmark)
 @Warmup(iterations = 2)
 @Measurement(iterations = 3)
 public class DigestBench {
@@ -26,14 +26,14 @@ public class DigestBench {
     @Param({"1024", "1048576", "536870912"})
     public int size;
 
-    private byte[] bytes;
+    private byte[] testBytes;
     private final byte[] outBytes = new byte[32];
 
     @Setup
     public void prepare() throws NoSuchAlgorithmException {
         digestInstance = MessageDigest.getInstance("MD5");
         blake3 = Blake3.initHash();
-        bytes = new byte[size];
+        testBytes = new byte[size];
     }
 
     @Setup(Level.Iteration)
@@ -44,34 +44,34 @@ public class DigestBench {
 
     @Benchmark
     public void digestMD5(Blackhole bh) {
-        bh.consume(digestInstance.digest(bytes));
+        bh.consume(digestInstance.digest(testBytes));
     }
 
     @Benchmark
     public void digestBlake(Blackhole bh) {
-        blake3.update(bytes);
+        blake3.update(testBytes);
         bh.consume(blake3.doFinalize(outBytes));
     }
 
     @Benchmark
     public void digestMD5_inc(Blackhole bh) throws IOException {
-        forEachBufferIn(bytes, (buffer, len) -> digestInstance.update(buffer, 0, len));
+        forEachBufferIn(testBytes, (buffer, len) -> digestInstance.update(buffer, 0, len));
         bh.consume(digestInstance.digest());
     }
 
     @Benchmark
     public void digestBlake_inc(Blackhole bh) throws IOException {
-        forEachBufferIn(bytes, (buffer, len) -> blake3.update(buffer, 0, len));
+        forEachBufferIn(testBytes, (buffer, len) -> blake3.update(buffer, 0, len));
         bh.consume(blake3.doFinalize(outBytes));
     }
 
     final int TRANSFER_BUFFER_SIZE = 16 * 1024;
-    byte[] buffer = new byte[TRANSFER_BUFFER_SIZE];
-    private void forEachBufferIn(byte[] bytes, BiConsumer<byte[], Integer> consumer) throws IOException {
-        try(var reader = new BufferedInputStream(new ByteArrayInputStream(bytes), TRANSFER_BUFFER_SIZE)){
+    byte[] readBuffer = new byte[TRANSFER_BUFFER_SIZE];
+    private void forEachBufferIn(byte[] allBytes, BiConsumer<byte[], Integer> consumer) throws IOException {
+        try(var reader = new BufferedInputStream(new ByteArrayInputStream(allBytes), TRANSFER_BUFFER_SIZE)){
             int nRead;
-            while ((nRead = reader.read(buffer, 0, TRANSFER_BUFFER_SIZE)) >= 0) {
-                consumer.accept(buffer, nRead);
+            while ((nRead = reader.read(readBuffer, 0, TRANSFER_BUFFER_SIZE)) >= 0) {
+                consumer.accept(readBuffer, nRead);
             }
         }
     }
