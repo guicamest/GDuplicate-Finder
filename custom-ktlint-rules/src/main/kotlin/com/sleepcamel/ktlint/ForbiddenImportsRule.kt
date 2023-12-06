68555b611433cfede54f850bce0b530935385665
guicamest <guicamest@gmail.com>
2023-12-06 22:22:19 +0100

    chore(ktlint): format `custom-ktlint-rules` project

--- custom-ktlint-rules/src/main/kotlin/com/sleepcamel/ktlint/ForbiddenImportsRule.kt
+++ custom-ktlint-rules/src/main/kotlin/com/sleepcamel/ktlint/ForbiddenImportsRule.kt
@@ -8,12 +8,12 @@ import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementTypes
 
 class ForbiddenImportsRule : Rule(
     ruleId = RuleId("$CUSTOM_RULE_SET_ID:forbidden"),
-    about = About()
+    about = About(),
 ) {
     override fun beforeVisitChildNodes(
         node: ASTNode,
         autoCorrect: Boolean,
-        emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit
+        emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit,
     ) {
         if (node.elementType == KtStubElementTypes.IMPORT_DIRECTIVE) {
             val importDirective = node.psi as KtImportDirective
--- custom-ktlint-rules/src/test/kotlin/com/sleepcamel/ktlint/ForbiddenImportsRuleTest.kt
+++ custom-ktlint-rules/src/test/kotlin/com/sleepcamel/ktlint/ForbiddenImportsRuleTest.kt
@@ -4,42 +4,44 @@ import com.pinterest.ktlint.test.KtLintAssertThat.Companion.assertThatRule
 import org.junit.jupiter.api.Test
 
 class ForbiddenImportsRuleTest {
-
     private val wrappingRuleAssertThat = assertThatRule { ForbiddenImportsRule() }
 
     @Test
     fun `import of java-io-File should report a violation`() {
-        val fileContent = """
+        val fileContent =
+            """
             import java.io.File
-        """.trimIndent()
+            """.trimIndent()
 
         wrappingRuleAssertThat(fileContent)
             .hasLintViolationWithoutAutoCorrect(
                 line = 1,
                 col = 1,
-                detail = "File has a forbidden import: java.io.File"
+                detail = "File has a forbidden import: java.io.File",
             )
     }
 
     @Test
     fun `import of java-io-File with alias should report a violation`() {
-        val fileContent = """
+        val fileContent =
+            """
             import java.io.File as MyFile
-        """.trimIndent()
+            """.trimIndent()
 
         wrappingRuleAssertThat(fileContent)
             .hasLintViolationWithoutAutoCorrect(
                 line = 1,
                 col = 1,
-                detail = "File has a forbidden import: java.io.File"
+                detail = "File has a forbidden import: java.io.File",
             )
     }
 
     @Test
     fun `import of java-nio-file should not report a violation`() {
-        val fileContent = """
+        val fileContent =
+            """
             import java.nio.file.*
-        """.trimIndent()
+            """.trimIndent()
 
         wrappingRuleAssertThat(fileContent).hasNoLintViolations()
     }
