87fe248a579fe9978f4af70181e950a2d5f79aae
guicamest <guicamest@gmail.com>
2023-12-18 10:16:23 +0100

    chore: Update content state as files are processed
    Revert "chore: Change type of `groupToProcess` of content state to `Collection<Collection<PathWithAttributes>>`"
    
    This reverts commit 4d3ad52474fc8661ed98f9de3c09fc6d05981ddd.

--- core/src/main/kotlin/com/sleepcamel/gduplicatefinder/core/PathGrouping.kt
+++ core/src/main/kotlin/com/sleepcamel/gduplicatefinder/core/PathGrouping.kt
@@ -1,10 +1,5 @@
 package com.sleepcamel.gduplicatefinder.core
 
-fun Collection<PathWithAttributes>.withSameContent() =
-    groupBy {
-        it.contentHash()
-    }.filter { (_, paths) -> paths.size > 1 }
-
 fun Map<String, Collection<PathWithAttributes>>.duplicateGroups(): Collection<DuplicateGroup> =
     map { (hash, paths) ->
         DuplicateGroup(hash = hash, paths = paths)
