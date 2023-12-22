202ff4b77600d07a54f3fc3f1622e535db558334
guicamest <guicamest@gmail.com>
2023-12-22 01:28:49 +0100

    chore(custom-ktlint-rules): make `val` also `const`

--- custom-ktlint-rules/src/main/kotlin/com/sleepcamel/ktlint/ForbiddenImportsRuleSetProvider.kt
+++ custom-ktlint-rules/src/main/kotlin/com/sleepcamel/ktlint/ForbiddenImportsRuleSetProvider.kt
@@ -4,7 +4,7 @@ import com.pinterest.ktlint.cli.ruleset.core.api.RuleSetProviderV3
 import com.pinterest.ktlint.rule.engine.core.api.RuleProvider
 import com.pinterest.ktlint.rule.engine.core.api.RuleSetId
 
-internal val CUSTOM_RULE_SET_ID = "custom-imports"
+internal const val CUSTOM_RULE_SET_ID = "custom-imports"
 
 class ForbiddenImportsRuleSetProvider : RuleSetProviderV3(RuleSetId(CUSTOM_RULE_SET_ID)) {
     override fun getRuleProviders(): Set<RuleProvider> = setOf(RuleProvider { ForbiddenImportsRule() })
