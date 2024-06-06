3c6a757cd6a2a6a89d2c7be4fb01084d6cd96a4c
guicamest <283778+guicamest@users.noreply.github.com>
2024-06-06 08:17:07 +0200

    chore(ForbiddenImportsRule): Use new `RuleAutocorrectApproveHandler` interface

--- custom-ktlint-rules/src/main/kotlin/com/sleepcamel/ktlint/ForbiddenImportsRule.kt
+++ custom-ktlint-rules/src/main/kotlin/com/sleepcamel/ktlint/ForbiddenImportsRule.kt
@@ -15,7 +15,9 @@
  */
 package com.sleepcamel.ktlint
 
+import com.pinterest.ktlint.rule.engine.core.api.AutocorrectDecision
 import com.pinterest.ktlint.rule.engine.core.api.Rule
+import com.pinterest.ktlint.rule.engine.core.api.RuleAutocorrectApproveHandler
 import com.pinterest.ktlint.rule.engine.core.api.RuleId
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
 import org.jetbrains.kotlin.psi.KtImportDirective
@@ -25,11 +27,11 @@ class ForbiddenImportsRule :
     Rule(
         ruleId = RuleId("$CUSTOM_RULE_SET_ID:forbidden"),
         about = About(),
-    ) {
+    ),
+    RuleAutocorrectApproveHandler {
     override fun beforeVisitChildNodes(
         node: ASTNode,
-        autoCorrect: Boolean,
-        emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit,
+        emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> AutocorrectDecision,
     ) {
         if (node.elementType == KtStubElementTypes.IMPORT_DIRECTIVE) {
             val importDirective = node.psi as KtImportDirective
